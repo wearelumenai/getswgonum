d294ec4076f5f109d220d88b9cf75a2fcb8646ab
ydarma <yves.darmaillac@gmail.com>
2019-04-18 18:36:09 +0200

    code upload

---
+++ spectral.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/csv"
+	"github.com/bugra/kmeans"
+	"gonum.org/v1/gonum/mat"
+	"log"
+	"os"
+	"strconv"
+)
+
+func LoadAdjacency(path string) (mat.Symmetric, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	edges, err := csv.NewReader(file).ReadAll()
+	A := CreateSparseMatrix(edges)
+	return A, err
+}
+
+func CreateSparseMatrix(edges [][]string) mat.Symmetric {
+	A := mat.NewSymDense(34, nil)
+	for _, edge := range edges {
+		start, _ := strconv.Atoi(edge[0])
+		end, _ := strconv.Atoi(edge[1])
+		if start > 34 || end > 34 {
+			panic("")
+		}
+		A.SetSym(start-1, end-1, 1.)
+	}
+	return A
+}
+
+func GetDegrees(A mat.Symmetric) mat.Diagonal {
+	n := A.Symmetric()
+	diag := make([]float64, n)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			diag[i] += A.At(i, j)
+		}
+	}
+	return mat.NewDiagDense(n, diag)
+}
+
+func GetLaplacian(A mat.Symmetric) mat.Symmetric {
+	n := A.Symmetric()
+	L := mat.NewSymDense(n, nil)
+	L.CopySym(A)
+	D := GetDegrees(A)
+	for i := 0; i < n; i++ {
+		l := L.At(i, i) - D.At(i, i)
+		L.SetSym(i, i, l)
+	}
+	return L
+}
+
+func GetSmallestEigenVectors(L mat.Symmetric, k int) mat.Matrix {
+	n := L.Symmetric()
+	decomposition := mat.EigenSym{}
+	decomposition.Factorize(L, true)
+	E := mat.NewDense(n, n, nil)
+	decomposition.VectorsTo(E)
+	return E.Slice(0, n, n-k, n)
+}
+
+func KMeans(E mat.Matrix, k int, iter int) ([]int, error) {
+	rows, _ := E.Dims()
+	rawData := make([][]float64, rows)
+	for i := 0; i < rows; i++ {
+		rawData[i] = mat.Row(nil, i, E)
+	}
+	return kmeans.Kmeans(rawData, k, kmeans.EuclideanDistance, iter)
+}
+
+func main() {
+	A, _ := LoadAdjacency("./karate.csv")
+	L := GetLaplacian(A)
+	E := GetSmallestEigenVectors(L, 4)
+	labels, _ := KMeans(E, 3, 10)
+	log.Print(labels)
+}
---
+++ spectral_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_LoadAdjacency(t *testing.T) {
+	A, err := LoadAdjacency("./karate.csv")
+	if err != nil {
+		t.Error("unexpected error", err)
+		return
+	}
+	rows, cols := A.Dims()
+	if rows != 34 || cols != 34 {
+		t.Error("dimension error")
+	}
+	if w := A.At(0, 11); w != 1. {
+		t.Error("expected 1 got", w)
+	}
+	if w := A.At(0, 29); w != 0. {
+		t.Error("expected 0 got", w)
+	}
+}
+
+func Test_GetDegrees(t *testing.T) {
+	A, _ := LoadAdjacency("./karate.csv")
+	D := GetDegrees(A)
+	if d := D.At(0, 0); d != 16 {
+		t.Error("expected 6 got", d)
+	}
+}
+
+func Test_GetLaplacian(t *testing.T) {
+	A, _ := LoadAdjacency("./karate.csv")
+	L := GetLaplacian(A)
+
+	if w := L.At(0, 11); w != 1. {
+		t.Error("expected 1 got", w)
+	}
+	if d := L.At(0, 0); d != -16 {
+		t.Error("expected 12 got", d)
+	}
+}
+
+func Test_GetSmallEigenVectors(t *testing.T) {
+	A, _ := LoadAdjacency("./karate.csv")
+	L := GetLaplacian(A)
+	E := GetSmallestEigenVectors(L, 2)
+	rows, cols := E.Dims()
+	if rows != 34 {
+		t.Error("expected n rows got", rows)
+	}
+	if cols != 2 {
+		t.Error("expected 2 rows got", cols)
+	}
+}
+
+func Test_KMeans(t *testing.T) {
+	A, _ := LoadAdjacency("./karate.csv")
+	L := GetLaplacian(A)
+	E := GetSmallestEigenVectors(L, 4)
+	labels, _ := KMeans(E, 2, 10)
+	l01 := labels[0]
+	l33 := labels[33]
+
+	if l01 == l33 {
+		t.Error("1 and 34 should not be in the same community")
+	}
+	if labels[2] != l01 {
+		t.Error("1 and 2 should not be in the same community")
+	}
+	if labels[32] != l33 {
+		t.Error("33 and 34 should not be in the same community")
+	}
+}
